114097f9900a42c4cd5156d46042ce605189494c
ironhead <iron_head@icloud.com>
2021-11-17 14:57:01 +0800

    test earth & rotation

--- src/main.js
+++ src/main.js
@@ -2,11 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import vuetify from './plugins/vuetify'
+// import AsyncComputed from 'vue-async-computed'
+import animate from 'animate.css'
 Vue.config.productionTip = false
+// Vue.use(AsyncComputed)
+Vue.use(animate)
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
