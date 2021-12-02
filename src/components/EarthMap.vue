f3c0a331caa8d201612eabe43c9d27f9a83a5956
ironhead <iron_head@icloud.com>
2021-12-02 17:13:35 +0800

    refactor the code

--- src/components/EarthMap.vue
+++ src/components/EarthMap.vue
@@ -11,6 +11,7 @@
 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { lglt2xyz } from '@/plugins/utils'
+import earthTexture from '@/assets/earth2.jpg'
 
 var THREE = require('three')
 export default {
@@ -21,14 +22,16 @@ export default {
     scene: undefined,
     camera: undefined,
     renderer: undefined,
-    group: undefined,
-    light: undefined,
-    controls: undefined
+    earthGroup: undefined,
+    lightGroup: undefined,
+    controls: undefined,
+    earthMesh: undefined
   }),
   mounted () {
     this.earthMapHeight = this.$el.offsetHeight
     this.earthMapWidth = this.$el.offsetWidth
     this.renderEarth(this.$refs.earth)
+    this.animate()
     var elementResizeDetectorMaker = require('element-resize-detector')
     var erd = elementResizeDetectorMaker()
     const that = this
@@ -43,6 +46,64 @@ export default {
       this.camera.aspect = this.earthMapWidth / this.earthMapHeight
       this.camera.updateProjectionMatrix()
       this.renderer.setSize(this.earthMapWidth, this.earthMapHeight)
+    },
+    initRenderer (elementDOM) {
+      const renderer = new THREE.WebGLRenderer({ canvas: elementDOM, alpha: true, antialias: true })
+      renderer.shadowMap.enabled = true
+      renderer.setSize(this.earthMapWidth, this.earthMapHeight)
+      this.renderer = renderer
+      return renderer
+    },
+    initCamera () {
+      const camera = new THREE.PerspectiveCamera(45, this.earthMapWidth / this.earthMapHeight, 0.1, 2000)
+      camera.position.z = 300
+      this.camera = camera
+      return camera
+    },
+    initScene () {
+      const scene = new THREE.Scene()
+      scene.background = new THREE.Color('#020924')
+      scene.fog = new THREE.Fog('#020924', 200, 1000)
+      this.scene = scene
+      return scene
+    },
+    initEarthGroup () {
+      const earthGroup = new THREE.Group()
+      this.earthGroup = earthGroup
+      return earthGroup
+    },
+    initLightGroup () {
+      const lightGroup = new THREE.Group()
+      const ambientLight = new THREE.AmbientLight(0xcccccc, 1.1)
+      lightGroup.add(ambientLight)
+      var directionalLight = new THREE.DirectionalLight(0xffffff, 0.2)
+      directionalLight.position.set(1, 0.1, 0).normalize()
+      lightGroup.add(directionalLight)
+      var directionalLight2 = new THREE.DirectionalLight(0xff2ffff, 0.2)
+      directionalLight2.position.set(1, 0.1, 0.1).normalize()
+      lightGroup.add(directionalLight2)
+      var hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444, 0.2)
+      hemiLight.position.set(0, 1, 0)
+      lightGroup.add(hemiLight)
+      var directionalLight3 = new THREE.DirectionalLight(0xffffff)
+      directionalLight3.position.set(1, 500, -20)
+      directionalLight3.castShadow = true
+      directionalLight3.shadow.camera.top = 18
+      directionalLight3.shadow.camera.bottom = -10
+      directionalLight3.shadow.camera.left = -52
+      directionalLight3.shadow.camera.right = 12
+      lightGroup.add(directionalLight3)
+      this.lightGroup = lightGroup
+      return lightGroup
+    },
+    initControls () {
+      const controls = new OrbitControls(this.camera, this.renderer.domElement)
+      controls.enableZoom = true
+      controls.enablePan = true
+      controls.enableRotate = true
+      controls.autoRotateSpeed = 2
+      this.controls = controls
+      return controls
     },
     initBackground () {
       const starPositions = []
@@ -61,77 +122,11 @@ export default {
       geometry.setAttribute('position', new THREE.Float32BufferAttribute(starPositions, 3))
       geometry.setAttribute('color', new THREE.Float32BufferAttribute(starColors, 3))
     },
-    renderEarth (elementDOM) {
-      let scene, camera, renderer, group, light, controls
-      const initRenderer = (elementDOM) => {
-        renderer = new THREE.WebGLRenderer({ canvas: elementDOM, alpha: true, antialias: true })
-        renderer.shadowMap.enabled = true
-        renderer.setSize(this.earthMapWidth, this.earthMapHeight)
-        this.renderer = renderer
-      }
-
-      const initCamera = () => {
-        camera = new THREE.Camera(45, this.earthMapWidth / this.earthMapHeight, 0.1, 2000)
-        camera.position.z = 300
-        this.camera = camera
-      }
-
-      const initScene = () => {
-        scene = new THREE.Scene()
-        scene.background = new THREE.Color('#020924')
-        scene.fog = new THREE.Fog('#020924', 200, 1000)
-        this.scene = scene
-      }
-
-      const initGroup = () => {
-        group = new THREE.Group()
-        this.group = group
-      }
-
-      const initLight = () => {
-        const ambientLight = new THREE.AmbientLight(0xcccccc, 1.1)
-        scene.add(ambientLight)
-        var directionalLight = new THREE.DirectionalLight(0xffffff, 0.2)
-        directionalLight.position.set(1, 0.1, 0).normalize()
-        var directionalLight2 = new THREE.DirectionalLight(0xff2ffff, 0.2)
-        directionalLight2.position.set(1, 0.1, 0.1).normalize()
-        scene.add(directionalLight)
-        scene.add(directionalLight2)
-        var hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444, 0.2)
-        hemiLight.position.set(0, 1, 0)
-        scene.add(hemiLight)
-        var directionalLight = new THREE.DirectionalLight(0xffffff)
-        directionalLight.position.set(1, 500, -20)
-        directionalLight.castShadow = true
-        directionalLight.shadow.camera.top = 18
-        directionalLight.shadow.camera.bottom = -10
-        directionalLight.shadow.camera.left = -52
-        directionalLight.shadow.camera.right = 12
-        scene.add(directionalLight)
-        // this.light = light
-      }
-
-      const initControls = () => {
-        controls = new OrbitControls(camera, renderer.domElement)
-        // controls.enableDamping = true
-        controls.enableZoom = true
-        controls.enablePan = true
-        controls.enableRotate = true
-        controls.autoRotateSpeed = 2
-        this.controls = controls
-      }
-
-      const rendering = () => {
-        renderer.clear()
-        renderer.render(scene, camera)
-      }
-
-      initRenderer(elementDOM)
-      initCamera()
-      initScene()
-      initGroup()
-      initLight()
-      initControls()
+    rendering () {
+      this.renderer.clear()
+      this.renderer.render(this.scene, this.camera)
+    },
+    initEarth () {
       var earthTexture = require('@/assets/earth2.jpg')
       var geometry = new THREE.SphereGeometry(100, 1000, 1000) // 球体
       var material = new THREE.MeshBasicMaterial({
@@ -139,16 +134,28 @@ export default {
         transparent: true
       })
       var mesh = new THREE.Mesh(geometry, material)
-      group.add(mesh)
-      scene.add(group)
-      var animate = function () {
-        if (controls) {
-          controls.update()
-        }
-        rendering()
-        requestAnimationFrame(animate)
+      this.earthMesh = mesh
+      return mesh
+    },
+    animate () {
+      if (this.controls) {
+        this.controls.update()
       }
-      animate()
+      this.rendering()
+      requestAnimationFrame(this.animate)
+    },
+    renderEarth (elementDOM) {
+      let scene, camera, renderer, earthGroup, lightGroup, controls
+      renderer = this.initRenderer(elementDOM)
+      camera = this.initCamera()
+      scene = this.initScene()
+      earthGroup = this.initEarthGroup()
+      lightGroup = this.initLightGroup()
+      scene.add(lightGroup)
+      controls = this.initControls()
+      const earthMesh = this.initEarth()
+      scene.add(earthGroup)
+      earthGroup.add(earthMesh)
     }
   }
 }
