0b804ba76bd85cb16ea8c88668e5328019d6717a
ironhead <iron_head@icloud.com>
2021-12-07 18:27:55 +0800

    try to test load data

--- src/plugins/utils.js
+++ src/plugins/utils.js
@@ -1,4 +1,7 @@
 export { BLH2XYZ }
+export { getEarthquakeRelations }
+export { getEarthquakeByDateAndMag }
+import earthquakeJson from '@/assets/earthquake_v2.json'
 
 function BLH2XYZ (lng, lat, radius) {
   const phi = (180 + lng) * (Math.PI / 180)
@@ -8,4 +11,36 @@ function BLH2XYZ (lng, lat, radius) {
     y: radius * Math.cos(theta),
     z: radius * Math.sin(theta) * Math.sin(phi)
   }
+}
+
+function getEarthquakeRelations(clickId) {
+  var showList = [];
+  var currentTag = earthquakeJson[clickId].tag;
+  if (currentTag == ""){
+    return showList;
+  }
+  for(var i = 0; i < earthquakeJson.length; i++){
+      if(earthquakeJson[i].tag == currentTag){
+          showList.push(earthquakeJson[i])
+      }
+  }
+  return showList;
+}
+
+function getEarthquakeByDateAndMag(startDate, endDate, minMagnitude, maxMagnitude) {
+  var earthquakes = [];
+  var start = new Date(startDate);
+  var end = new Date(endDate);
+  for(var i = 0; i < earthquakeJson.length; i++){
+    var date = new Date(earthquakeJson[i].date);
+    if(date > end){
+      break;
+    }
+    if(date >= start){
+      if(earthquakeJson[i].magnitude >= minMagnitude && earthquakeJson[i].magnitude <= maxMagnitude){
+        earthquakes.push(earthquakeJson[i])
+      }
+    }
+  }
+  return earthquakes;
 }
