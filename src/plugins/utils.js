f2bb77cba994adca12c2349e217ebb5233fdad8a
ironhead <iron_head@icloud.com>
2021-12-07 22:53:02 +0800

    add tooltip

--- src/plugins/utils.js
+++ src/plugins/utils.js
@@ -2,7 +2,6 @@ import earthquakeJson from '@/assets/earthquake_v2.json'
 export { BLH2XYZ }
 export { getEarthquakeRelations }
 export { getEarthquakeByDateAndMag }
-import earthquakeJson from '@/assets/earthquake_v2.json'
 
 function BLH2XYZ (lng, lat, radius) {
   const phi = (180 + lng) * (Math.PI / 180)
@@ -30,8 +29,8 @@ function getEarthquakeRelations (clickId) {
 
 function getEarthquakeByDateAndMag (startDate, endDate, minMagnitude, maxMagnitude) {
   var earthquakes = []
-  var start = new Date(startDate)
-  var end = new Date(endDate)
+  var start = new Date(startDate + ' 00:00:00')
+  var end = new Date(endDate + ' 23:59:59')
   for (let i = 0; i < earthquakeJson.length; i++) {
     const date = new Date(earthquakeJson[i].date)
     if (date > end) {
@@ -45,4 +44,3 @@ function getEarthquakeByDateAndMag (startDate, endDate, minMagnitude, maxMagnitu
   }
   return earthquakes
 }
-
